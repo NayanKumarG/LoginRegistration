73b71eff445ec7424f52dbee3bcc222cbd354806
NayanKumarG <nayangkumar@gmail.com>
2020-02-12 11:06:35 +0530

    [Nayan] Add. Servlet implementation to accept client login request

--- ServletLoginRegistration/src/main/java/com/bridgelabz/controller/Login.java
+++ ServletLoginRegistration/src/main/java/com/bridgelabz/controller/Login.java
@@ -7,10 +7,8 @@
 
 package com.bridgelabz.controller;
 import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,7 +21,7 @@ import com.bridgelabz.serviceimpl.LoginImpl;
 /*
  * Servlet implementation class Login
  */
-@WebServlet("/login")
+//@WebServlet("/login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	static LoginModel lmodel = new LoginModel();
