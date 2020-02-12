d0737b8ea02907c823bdd1f2d5acb32ae5c7ecc4
NayanKumarG <nayangkumar@gmail.com>
2020-02-12 11:13:52 +0530

    [Nayan] Add. interface to fetch the data

---
+++ ServletLoginRegistration/src/main/java/com/bridgelabz/service/LoginInf.java
@@ -0,0 +1,10 @@
+/*
+ * created by:Nayan Kumar
+ * purpose:interface contain one method to fetch the data from database
+ */
+package com.bridgelabz.service;
+import com.bridgelabz.model.LoginModel;
+
+public interface LoginInf {
+boolean fetchData(LoginModel lmodel);
+}
