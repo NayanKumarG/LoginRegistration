014136e4158e84436d5ed57e98aacd78d8e2495b
NayanKumarG <nayangkumar@gmail.com>
2020-02-12 11:11:43 +0530

    [Nayan] Add. model class for registration details

---
+++ ServletLoginRegistration/src/main/java/com/bridgelabz/model/RegistrationModel.java
@@ -0,0 +1,53 @@
+/*
+ * created by:Nayan Kumar
+ * purpose:Model class to set the registration detail and returns
+ */
+package com.bridgelabz.model;
+public class RegistrationModel {
+
+	String name;
+	String password;
+	String mail;
+	String city;
+	int age;
+	String phno;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getPhno() {
+		return phno;
+	}
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	
+	
+}
