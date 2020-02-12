1ebb2d96972e3509bcd45b4ff3be9d4e6971d1e3
NayanKumarG <nayangkumar@gmail.com>
2020-02-12 11:14:25 +0530

    [Nayan] Add. interface to save the data

---
+++ ServletLoginRegistration/src/main/java/com/bridgelabz/service/RegistrationInf.java
@@ -0,0 +1,10 @@
+/*
+ * created by:Nayan Kumar
+ * purpose:interface having single method to save data into database
+ */
+package com.bridgelabz.service;
+import com.bridgelabz.model.RegistrationModel;
+
+public interface RegistrationInf {
+void saveData(RegistrationModel rmodel);
+}
