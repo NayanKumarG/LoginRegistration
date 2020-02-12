7d943ace9508daf862cfca8ee208e46815c7fa98
NayanKumarG <nayangkumar@gmail.com>
2020-02-12 10:08:01 +0530

    [Nayan] Add. Servlet implementation to accept client login request

---
+++ ServletLoginRegistration/src/main/java/com/bridgelabz/controller/Login.java
@@ -0,0 +1,56 @@
+/*
+ * created by:Nayan Kumar
+ * 
+ * purpose:controller for login
+ * 
+ */
+
+package com.bridgelabz.controller;
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.bridgelabz.model.LoginModel;
+import com.bridgelabz.service.LoginInf;
+import com.bridgelabz.serviceimpl.LoginImpl;
+
+/*
+ * Servlet implementation class Login
+ */
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	static LoginModel lmodel = new LoginModel();
+	static LoginInf linf = new LoginImpl();
+
+	/*
+	 * overriding doPost method
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		lmodel.setName(name);
+		lmodel.setPassword(password);
+		boolean b = linf.fetchData(lmodel);
+		if(b)
+		{
+			HttpSession session = request.getSession();
+			session.setAttribute("username", name);
+			RequestDispatcher rd = request.getRequestDispatcher("welcome.jsp");
+			rd.forward(request, response);
+		}
+		else
+		{
+			RequestDispatcher rd = request.getRequestDispatcher("Login.jsp");
+			rd.forward(request, response);
+		}	
+	}
+}
