4a71c7e1306e5c1993b6884b890ee54fdec4afca
NayanKumarG <nayangkumar@gmail.com>
2020-02-12 11:11:09 +0530

    [Nayan] Add. model class for login details

---
+++ ServletLoginRegistration/src/main/java/com/bridgelabz/model/LoginModel.java
@@ -0,0 +1,23 @@
+/*
+ * created by:Nayan Kumar
+ * purpose:class to set the login detail and returns
+ */
+package com.bridgelabz.model;
+
+public class LoginModel {
+String name;
+String password;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+	
+}
