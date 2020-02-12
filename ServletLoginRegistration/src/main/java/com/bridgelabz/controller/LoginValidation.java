feb0012b7395f8758f68cedd35ef777a77d99d3d
NayanKumarG <nayangkumar@gmail.com>
2020-02-12 10:11:03 +0530

    [Nayan] Add. Servlet filter to validate login detail

---
+++ ServletLoginRegistration/src/main/java/com/bridgelabz/controller/LoginValidation.java
@@ -0,0 +1,44 @@
+/*
+ * created by:Nayan Kumar
+ * purpose:servlet filter to validate login detail
+ */
+package com.bridgelabz.controller;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/*
+ * Servlet Filter implementation class LoginValidation
+ */
+@WebFilter("/login")
+public class LoginValidation implements Filter {
+
+	/*
+	 * passing request after validation using chain
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+		PrintWriter out = response.getWriter();
+		HttpServletRequest req = (HttpServletRequest)request;
+		String uname = req.getParameter("name");
+		String password = req.getParameter("password");
+		if(uname.equals("") || password.equals(""))
+
+			out.println("Fill all the fields");
+
+		else
+
+			chain.doFilter(request, response);
+
+	}
+
+
+
+}
