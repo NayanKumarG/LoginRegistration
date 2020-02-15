3f4d0f8edfe5817d95900b02e5f98d36d99e9433
NayanKumarG <nayangkumar@gmail.com>
2020-02-15 18:26:12 +0530

    [Nayan] Add. implementation class to access user detail from database

---
+++ ServletLoginRegistration/src/main/java/com/bridgelabz/serviceimpl/UsersDao.java
@@ -0,0 +1,90 @@
+/*
+ * created by:Nayan Kumar
+ * 
+ * purpose:implementation to fetch the data from database
+ * 
+ */
+package com.bridgelabz.serviceimpl;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.bridgelabz.model.Users;
+import com.bridgelabz.service.UserInf;
+
+public class UsersDao implements UserInf{
+private static Logger log = LogManager.getLogger(UsersDao.class);
+	
+	/*
+	 * method which returns the list of user
+	 */
+	@Override
+	public List<Users> getUsers(int start , int total) {
+		List<Users> users = new ArrayList<Users>();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String qry = ("select * from mydatabase.userRegister limit "+(start-1)+","+total);
+		try
+		{
+
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306?useSSL=false","nayan","nayan");
+			pstmt = con.prepareStatement(qry);
+		    rs = pstmt.executeQuery();
+		    while(rs.next())
+		    {
+		    	Users user = new Users();
+		    	user.setName(rs.getString(1));
+		    	user.setMailid(rs.getString(3));
+		    	user.setCity(rs.getString(4));
+		    	user.setAge(rs.getInt(5));
+		    	user.setPhno(rs.getString(6));
+		    	users.add(user);
+		    }
+		}catch(SQLException e)
+		{
+			log.error("connection not possible");
+		}finally
+		{
+			if(rs!=null)
+			{
+				try
+				{
+					rs.close();
+				}catch(SQLException e)
+				{
+					log.error(e);
+				}
+			}
+			if(pstmt!=null)
+			{
+				try
+				{
+					pstmt.close();
+				}catch(SQLException e)
+				{
+				log.error(e);
+				}
+			}
+			if(con!=null)
+			{
+				try
+				{
+					con.close();
+				}catch(SQLException e)
+				{
+					log.error(e);
+				}
+			}
+		}
+		return users;
+	}
+
+}
