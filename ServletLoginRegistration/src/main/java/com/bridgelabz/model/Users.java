d6a59890f9038c96da82dac44e631c61e263d07e
NayanKumarG <nayangkumar@gmail.com>
2020-02-15 18:24:09 +0530

    [Nayan] Add. model to set the user detail present in database

---
+++ ServletLoginRegistration/src/main/java/com/bridgelabz/model/Users.java
@@ -0,0 +1,49 @@
+/*
+ * created by:Nayan Kumar
+ * 
+ * purpose:Sets the details of users when fetching from database
+ * 
+ */
+package com.bridgelabz.model;
+public class Users {
+
+	
+	private String name;
+	private String mailid;
+	private String city;
+	private int age;
+	private String phno;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMailid() {
+		return mailid;
+	}
+	public void setMailid(String mailid) {
+		this.mailid = mailid;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getPhno() {
+		return phno;
+	}
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+	
+	
+	
+}
