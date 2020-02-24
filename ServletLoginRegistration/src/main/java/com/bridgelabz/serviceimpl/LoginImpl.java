40f4f3badcc0b9c3a9f2d46b3132ccea4fd13f94
NayanKumarG <nayangkumar@gmail.com>
2020-02-24 09:38:47 +0530

    [Nayan] Add. Database connection from dao class

--- ServletLoginRegistration/src/main/java/com/bridgelabz/serviceimpl/LoginImpl.java
+++ ServletLoginRegistration/src/main/java/com/bridgelabz/serviceimpl/LoginImpl.java
@@ -3,8 +3,6 @@
  * purpose:to fetch data from database
  */
 package com.bridgelabz.serviceimpl;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,12 +10,12 @@ import java.sql.SQLException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.bridgelabz.dao.DBConnection;
 import com.bridgelabz.model.LoginModel;
 import com.bridgelabz.service.LoginInf;
 
 public class LoginImpl implements LoginInf{
 	private static Logger log = LogManager.getLogger(LoginImpl.class);
-	Connection con = null;
 	PreparedStatement pstmt=null;
 	ResultSet rs = null;
 	String qry = "select * from mydatabase.userRegister where name=? and password=?";
@@ -29,8 +27,7 @@ public class LoginImpl implements LoginInf{
 	public boolean fetchData(LoginModel lmodel) {
 		try
 		{
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306?useSSL=false","root","root");
-			pstmt=con.prepareStatement(qry);
+			pstmt=DBConnection.getConnection().prepareStatement(qry);
 			pstmt.setString(1, lmodel.getName());
 			pstmt.setString(2,lmodel.getPassword());
 			rs=pstmt.executeQuery();
@@ -65,20 +62,12 @@ public class LoginImpl implements LoginInf{
 				{
 				log.error(e);
 				}
-			}
-			if(con!=null)
-			{
-				try
-				{
-					con.close();
-				}catch(SQLException e)
-				{
-					log.error(e);
-				}
-			}
+	
 		}
+		
+		
+	}
 		return false;
 
-
-	}
+}
 }
